900a085d4c24b522da06b3cc2f0ca158a5d4b173
fro5tin <rcardenas@daniloblackusa.com>
2010-09-12 14:12:38 -0500

    dashboard agencias

---
+++ sites/all/modules/motores/motores_anuncio/myaccount_engine.js
@@ -0,0 +1,9 @@
+Drupal.behaviors.myaccount = function()
+{
+  var rowspan = $('.view-micuenta tbody tr').length;
+  $('td.views-field-phpcode-1:first').attr('rowspan',rowspan);
+  $('td.views-field-phpcode-1:not(td.views-field-phpcode-1:first)').remove();
+  
+  $('td.views-field-phpcode-2:first').attr('rowspan',rowspan);
+  $('td.views-field-phpcode-2:not(td.views-field-phpcode-2:first)').remove();
+}
