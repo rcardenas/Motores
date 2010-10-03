c71cdf1128d1f41e2ce223d072d5945e3af425f3
fro5tin <rcardenas@daniloblackusa.com>
2010-10-03 14:28:46 -0500

    views motores anuncio

--- sites/all/modules/motores/motores_custom/taxonomy_engine.js
+++ sites/all/modules/motores/motores_custom/taxonomy_engine.js
@@ -3,44 +3,78 @@ Drupal.behaviors.motoresTaxo = function (context) {
 	var valores = [];
 	var alternativo = [];
 	var llave = '';
-	var html = '<select id="valores-primer" class="primer-nivel" name="primer">'
+	var html = '<select id="valores-primer" size="10" class="primer-nivel" name="primer">'
 
-	$('#edit-field-marca-value > option').each(function() {
-	   if($(this).text().charAt(0)!=' ' && $(this).text().charAt(0)!='-'){
+  /** 
+   * Configurar datos y arreglos temporales
+   */
+	$('#edit-field-marca-value > option').each(function() 
+	{
+	   if($(this).text().charAt(0)!=' ' && $(this).text().charAt(0)!='-')
+	   {
 		    llave=$(this).text();
 		    valores[llave]=[];
 		    html = html + '<option value="'+llave+'">'+llave+'</option>';
 		    var valor_al = new Array();
-			valor_al['text']=$(this).text();
-			valor_al['llave']=$(this).val();
+  			valor_al['text']=$(this).text();
+  			valor_al['llave']=$(this).val();
 		    alternativo[llave]=valor_al;
 		    count++;
 	   }	
-	   if($(this).text().charAt(0)==' '){
-		 var valor = new Array();
-		 valor['text']=$(this).text();
-		 valor['llave']=$(this).val();
+	   if($(this).text().charAt(0)==' ')
+	   {
+  		 var valor = new Array();
+  		 valor['text']=$(this).text();
+  		 valor['llave']=$(this).val();
 	     valores[llave].push(valor);	
 	   }
 	});
-	
 	$('#edit-field-marca-value').hide();
+	$('#edit-field-marca-value').attr('size','10');
+	$('#edit-field-marca-value').removeAttr('multiple');
 	html = html + '</select>';
 	$('#edit-field-marca-value').before(html);
 
-    $('#valores-primer').change(function(i){
+  /** 
+   * Seleccionar la primer opcion al cargarse
+   */
+  $('#edit-field-marca-value').children().remove();
+  var src = $("option:first", '#valores-primer').val();
+  var swit = 1;
+  
+  $.each(valores[src],function(index,value)
+  {
+  	swit=0;
+  	$('#edit-field-marca-value').append('<option value="'+value['llave']+'">'+value['text']+'</option>');	
+  });
+  
+  if(swit==1)
+  {
+    $('#edit-field-marca-value').append('<option value="'+alternativo[src]['llave']+'">'+alternativo[src]['text']+'</option>');	
+  }
+  $('#edit-field-marca-value').show();
+  
+  
+  /** 
+   * Seleccionar otro menu on change
+   */ 
+  $('#valores-primer').change(function(i)
+  {
 	  $('#edit-field-marca-value').children().remove();
 	  var src = $("option:selected", this).val();
 	  var swit = 1;
-	  $.each(valores[src],function(index,value){
-		swit=0;
-		$('#edit-field-marca-value').append('<option value="'+value['llave']+'">'+value['text']+'</option>');	
+	  
+	  $.each(valores[src],function(index,value)
+	  {
+  		swit=0;
+  		$('#edit-field-marca-value').append('<option value="'+value['llave']+'">'+value['text']+'</option>');	
 	  });
-	  if(swit==1){
+	  
+	  if(swit==1)
+	  {
 	    $('#edit-field-marca-value').append('<option value="'+alternativo[src]['llave']+'">'+alternativo[src]['text']+'</option>');	
 	  }
 	  $('#edit-field-marca-value').show();
-	  
-    });
+  });
 
 }
